0f87b39d3672c59e5dae91cf3c1765ede617548a
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-02 12:53:58 -0600

    troubleshooting giphy api calls

--- client/layout.js
+++ client/layout.js
@@ -1,20 +1,15 @@
 Template.layout.helpers({
   'currentWeather': function() {
     var city = Session.get('city');
-    Meteor.call('getWeather', city, function(err, results) {
+//  CALLING API TOO MUCH... returning string to work on the giphy part
+//    Meteor.call('getWeather', city, function(err, results) {
 //      console.log(results.content);
-      Session.set('weather', JSON.parse(results.content).current_observation.weather);
-    });
-    forecast = Session.get('weather');
-    return (forecast + ' in ' + city).toUpperCase();
-  },
-  'weatherGif': function() {
-    Meteor.call('getWeatherGif', city, function(err, results) {
-      console.log(results.content);
-      Session.set('gif', JSON.parse(results.content))
-    });
-    var gifString = Session.get('gif')
-    return gifString;
+//      Session.set('weather', JSON.parse(results.content).current_observation.weather);
+//    });
+//    forecast = Session.get('weather');
+//    return (forecast + ' in ' + city).toUpperCase();
+      forecast = 'OVERCAST';
+      return 'OVERCAST';
   }
 });
 

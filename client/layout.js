5fb2a2b18e48acc71387c1caffb2b64d8e7de388
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-02 01:19:52 -0600

    changed forecast to global var so you can pass it to server

--- client/layout.js
+++ client/layout.js
@@ -1,25 +1,23 @@
 Template.layout.helpers({
-  'jweather' : function() {
+  'currentWeather': function() {
     var city = Session.get('city');
     Meteor.call('getWeather', city, function(err, results) {
-      console.log(results.content);
+//      console.log(results.content);
       Session.set('weather', JSON.parse(results.content).current_observation.weather);
     });
-    return (Session.get('weather') + ' in ' + city).toUpperCase();
+    forecast = Session.get('weather');
+    return (forecast + ' in ' + city).toUpperCase();
+  },
+  'weatherGif': function() {
+    Meteor.call('getWeatherGif', city, function(err, results) {
+      console.log(results.content);
+      Session.set('gif', JSON.parse(results.content))
+    });
+    var gifString = Session.get('gif')
+    return gifString;
   }
 });
 
-//Template.layout.currentWeather=function(){
-//  var city = Session.get('city');
-//  Meteor.call('getWeather', city, function(err, results){
-//    console.log(results.content);
-//    Session.set('weather', JSON.parse(results.content).weather[0]);
-//  });
-//  return (Session.get('weather') + 'in' + city).toUpperCase();
-//};
-
-//the following function sets the value of city from the html template
-//to the city set in the session to use in the query
 Template.layout.events({
   'change .cities':function(event, template){
     Session.set('city', template.find('.cities').value);

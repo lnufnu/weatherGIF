bc28a8eaf1a8864cb4709cab9766e7e5c784ea55
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-03 13:58:00 -0600

    now layout just returns call to wunderground and displays it

--- client/layout.js
+++ client/layout.js
@@ -8,30 +8,31 @@ Template.layout.helpers({
   //    });
   //    forecast = Session.get('weather');
   //    return (forecast + ' in ' + city).toUpperCase();
-        forecast = 'RAINY';
-        return 'RAINY';
+        forecast = 'SUNNY';
+        return 'SUNNY';
     }
   });
 
 
 Template.layout.events({
-    'submit .searchCity': function (event) {
-//        event.preventDefault();
-        console.log("wont ever change");
-        console.log(event.target);
-        console.log(event.target[0].value);
-        console.log(event.target[2].value);
+  'submit .searchCity': function (event) {
+    event.preventDefault();
+    console.log("wont ever change");
+    console.log(event.target);
+    console.log(event.target[0].value);
+    console.log(event.target[1].value);
+    console.log(event.target.cities.value);
 
-        event.target[0].value = Session.set('city');
-        event.target[2].value = Session.set('state');
+//        event.target[0].value = Session.set('city');
+//        event.target[1].value = Session.set('state');
 
-        console.log(Session.get('city'));
-  //      console.log(Session.get('city'));
-        console.log(Session.get('state'));
+    console.log(Session.get('city'));
+//      console.log(Session.get('city'));
+    console.log(Session.get('state'));
 
-        event.target[0].value = "";
-        event.target[2].value = "";
-    }
+//      event.target[0].value = "";
+//      event.target[2].value = "";
+  }
 //  'change .cities':function(event, template){
 //    Session.set('city', template.find('.cities').value);
 //  }

33c8f87ff451bdd5d6dd0fc79f42085bf4c564bf
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-03 12:44:57 -0600

    added div to style later

--- client/gifs.js
+++ client/gifs.js
@@ -2,12 +2,12 @@ if (Meteor.isClient) {
   Template.gifs.helpers ({
       'weatherGif': function() {
   //      var city = Session.get('city');
-  //      had this function taking city as param...not sure why
+  //      had this function taking city as param...may use later
+  //      with different API
         Meteor.call('getWeatherGif', forecast, function(err, results) {
           parsed = JSON.parse(results.content);
           Session.set('gif', parsed.data[0].embed_url);
         });
-        console.log(Session.get('gif'));
         var gif = Session.get('gif');
         return ('<iframe src="' + gif + '">');
       }

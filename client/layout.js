d4e5132acc7c4f9382e8a9658b7f93a1f65d0a17
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-03 12:40:38 -0600

    added event listener for form submit, inconsistent results

--- client/layout.js
+++ client/layout.js
@@ -1,40 +1,38 @@
 Template.layout.helpers({
-  'currentWeather': function() {
-    var city = Session.get('city');
-//  CALLING API TOO MUCH... returning string to work on the giphy part
-//    Meteor.call('getWeather', city, function(err, results) {
-//      console.log(results.content);
-//      Session.set('weather', JSON.parse(results.content).current_observation.weather);
-//    });
-//    forecast = Session.get('weather');
-//    return (forecast + ' in ' + city).toUpperCase();
-      forecast = 'CLOUDY';
-      return 'CLOUDY';
-  }
-});
+    'currentWeather': function() {
+      var city = Session.get('city');
+  //  CALLING API TOO MUCH... returning string to work on the giphy part
+  //    Meteor.call('getWeather', city, function(err, results) {
+  //      console.log(results.content);
+  //      Session.set('weather', JSON.parse(results.content).current_observation.weather);
+  //    });
+  //    forecast = Session.get('weather');
+  //    return (forecast + ' in ' + city).toUpperCase();
+        forecast = 'RAINY';
+        return 'RAINY';
+    }
+  });
+
 
 Template.layout.events({
+    'submit .searchCity': function (event) {
+//        event.preventDefault();
+        console.log("wont ever change");
+        console.log(event.target);
+        console.log(event.target[0].value);
+        console.log(event.target[2].value);
 
-  "submit .searchCities": function (event) {
+        event.target[0].value = Session.set('city');
+        event.target[2].value = Session.set('state');
 
-    // Prevent default browser form submit
-    event.preventDefault();
+        console.log(Session.get('city'));
+  //      console.log(Session.get('city'));
+        console.log(Session.get('state'));
 
-    // Get value from form element
-    var text = event.target.text.value;
-
-    // Insert a task into the collection - need to write function to pass it to
-    // getWeather in server.js
-    Tasks.insert({
-      text: text,
-      createdAt: new Date() // current time
-    });
-
-    // Clear form
-    event.target.text.value = "";
-  }
-
-  'change .cities':function(event, template){
-    Session.set('city', template.find('.cities').value);
-  }
+        event.target[0].value = "";
+        event.target[2].value = "";
+    }
+//  'change .cities':function(event, template){
+//    Session.set('city', template.find('.cities').value);
+//  }
 });

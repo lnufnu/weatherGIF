1ceec9f5f36eb206542a2127c1286787376164f5
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-02 20:59:00 -0600

    simplified

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 Meteor.methods({
-    'getWeather' : function(city){
+    'getWeather': function(city){
       return Meteor.http.call('GET', 'http://api.wunderground.com/api/8102e9fef9e3a203/conditions/q/TX/' + city + '.json');
-    }
-})
+    },
 
-Meteor.methods({
-    'getWeatherGif':function(forecast){
-      return Meteor.http.call('GET', 'http://api.giphy.com/v1/gifs/search?q=' + forecast + '&api_key=dc6zaTOxFJmzC');
+    'getWeatherGif': function(forecast){
+      var giphyKey = '&api_key=dc6zaTOxFJmzC';
+      return Meteor.http.call('GET', 'http://api.giphy.com/v1/gifs/search?q=' + forecast + giphyKey);
     }
-})
-//need to create corresponding endpoints and functions and define forecast object
+});

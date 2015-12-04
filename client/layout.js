b1b4ed6b302a4886492bc58de2b3c76b89ed9ab5
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-04 12:36:17 -0600

    currentWeather now takes state arg

--- client/layout.js
+++ client/layout.js
@@ -1,8 +1,9 @@
 Template.layout.helpers({
     'currentWeather': function() {
       var city = Session.get('city');
+      var state = Session.get('state');
   //  CALLING API TOO MUCH... returning string to work on the giphy part
-  //    Meteor.call('getWeather', city, function(err, results) {
+  //    Meteor.call('getWeather', city, state, function(err, results) {
   //      console.log(results.content);
   //      Session.set('weather', JSON.parse(results.content).current_observation.weather);
   //    });

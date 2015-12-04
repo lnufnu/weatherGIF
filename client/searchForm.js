d7b30ecc5d1f958d70bb9e2cbe3e781e63a3e011
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-03 22:01:02 -0600

    search form for cities works

--- client/searchForm.js
+++ client/searchForm.js
@@ -1,23 +1,17 @@
-Template.searchForm.events({
-  'submit .searchCity': function (event) {
-    event.preventDefault();
-    console.log("wont ever change");
-    console.log(event.target);
-    console.log(event.target[0].value);
-    console.log(event.target[1].value);
-    console.log(event.target.cities.value);
-
-//        event.target[0].value = Session.set('city');
-//        event.target[1].value = Session.set('state');
-
-    console.log(Session.get('city'));
-//      console.log(Session.get('city'));
-    console.log(Session.get('state'));
-
-//      event.target[0].value = "";
-//      event.target[2].value = "";
-  }
-//  'change .cities':function(event, template){
-//    Session.set('city', template.find('.cities').value);
+//Template.searchForm.helpers ({
+//  'submit .searchCity': function(event){
+//    Session.set('city', event.target[0].value);
+//    console.log(Session.get('city'));
 //  }
+//});
+
+Template.searchForm.events({
+  "submit form": function (event, template) {
+    event.preventDefault();
+    Session.set('city', event.target.cities.value);
+    Session.set('state', event.target.states.value);
+    console.log( Session.get('city'), Session.get('state') );
+    event.target.cities.value = "";
+    event.target.states.value = "";
+  }
 });

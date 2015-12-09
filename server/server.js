6a8c80529ae3cad9c39e1f12f653d640398eb68b
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-08 23:43:44 -0600

    required fibers, made getWeather synchronous

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
+var Future = Npm.require( 'fibers/future' );
+
 Meteor.methods({
+
   'getWeather': function(city, state){
-    return Meteor.http.call('GET', 'http://api.wunderground.com/api/8102e9fef9e3a203/conditions/q/'+ state + '/' + city + '.json');
+    var future = new Future();
+    console.log(city);
+    console.log(state);
+    HTTP.get('http://api.wunderground.com/api/8102e9fef9e3a203/conditions/q/' + state + '/' + city + '.json', {}, function (error, response) {
+      if (error) {
+        future.return (error);
+      } else {
+        future.return (response);
+      }
+    });
+  return future.wait();
   },
 
   'getWeatherGif': function(forecast){

2383a015d746fc26f1230ae27ad75d72778a8d22
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-09 14:51:22 -0600

    cleaned up

--- client/gifs.js
+++ client/gifs.js
@@ -1,15 +1,11 @@
-if (Meteor.isClient) {
-  Template.gifs.helpers ({
-      'weatherGif': function() {
-  //      var city = Session.get('city');
-  //      had this function taking city as param...may use later
-  //      with different API
-        Meteor.call('getWeatherGif', forecast, function(err, results) {
-          parsed = JSON.parse(results.content);
-          Session.set('gifPath', parsed.data[0].embed_url);
-        });
-        var gifPath = Session.get('gifPath');
-        return gifPath;
-      }
-  });
-}
+Template.gifs.helpers ({
+  'weatherGif': function() {
+    var forecast = Session.get('weather');
+    Meteor.call('getWeatherGif', forecast, function(err, results) {
+      parsed = JSON.parse(results.content);
+      Session.set('gifPath', parsed.data[0].embed_url);
+    });
+    var gifPath = Session.get('gifPath');
+    return gifPath;
+    }
+});

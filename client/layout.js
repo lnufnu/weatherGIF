2d4254b0708fd33d4be79164058f27b31d9dbd31
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-08 23:45:15 -0600

    started function to check for undef value on load

--- client/layout.js
+++ client/layout.js
@@ -1,16 +1,31 @@
 Template.layout.helpers({
+  'setDefault': function(forecast) {
+    if (forecast === undefined)
+      return 'YOUR WEATHER';
+    else
+      return forecastString;
+  },
   'currentWeather': function() {
-    console.log('city');
-    console.log('state');
+    var city = Session.get('city');
+    var state = Session.get('state');
+    console.log(city);
+    console.log(state);
 
 //  CALLING API TOO MUCH... returning string to work on the giphy part
-//    Meteor.call('getWeather', city, state, function(err, results) {
-//      console.log(results.content);
-//      Session.set('weather', JSON.parse(results.content).current_observation.weather);
-//    });
-//    forecast = Session.get('weather');
-//    return (forecast + ' in ' + city).toUpperCase();
-    forecast = 'SUNNY';
-    return 'SUNNY';
-  }
+    Meteor.call('getWeather', city, state, function(err, results) {
+      console.log(results.content);
+      Session.set('weather', JSON.parse(results.content).current_observation.weather);
+    });
+    var forecast = Session.get('weather');
+    return (forecast + ' in ' + city).toUpperCase();
+//    forecast = 'SUNNY';
+//    return 'SUNNY';
+  },
+  'setForcast': function (forecast) {
+    forcast: "somestring not undefine dont display undedined godammnit"
+    forcast:
+  setforecastObject: forcast;
+}
 });
+
+{{forecast}}

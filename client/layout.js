13f9c8a52b3de67e4555d1c59e55bb2a687b8107
Bethany Fort <lnufnu@users.noreply.github.com>
2015-12-09 14:48:34 -0600

    setDefault now avoids default values & currentWeather calls server

--- client/layout.js
+++ client/layout.js
@@ -1,31 +1,19 @@
 Template.layout.helpers({
-  'setDefault': function(forecast) {
-    if (forecast === undefined)
+  'setDefault': function() {
+    var city = Session.get('city');
+    var forecast = Session.get('weather');
+    if (forecast === undefined) {
       return 'YOUR WEATHER';
-    else
-      return forecastString;
+    } else {
+      return (forecast + ' in ' + city).toUpperCase();
+    }
   },
   'currentWeather': function() {
     var city = Session.get('city');
     var state = Session.get('state');
-    console.log(city);
-    console.log(state);
 
-//  CALLING API TOO MUCH... returning string to work on the giphy part
     Meteor.call('getWeather', city, state, function(err, results) {
-      console.log(results.content);
       Session.set('weather', JSON.parse(results.content).current_observation.weather);
     });
-    var forecast = Session.get('weather');
-    return (forecast + ' in ' + city).toUpperCase();
-//    forecast = 'SUNNY';
-//    return 'SUNNY';
   },
-  'setForcast': function (forecast) {
-    forcast: "somestring not undefine dont display undedined godammnit"
-    forcast:
-  setforecastObject: forcast;
-}
 });
-
-{{forecast}}
